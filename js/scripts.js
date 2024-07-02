9dcb74c9fcd7a6340c959c06d49c6f472a80cab4
vizssouza <vini.nelso@gmail.com>
2024-07-01 22:20:07 -0300

    Adicionando arquivos iniciais
    Falta Medias Querys de tudo e outras sessões da página

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// HAMBURGUER
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
